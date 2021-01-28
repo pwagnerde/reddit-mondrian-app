2bce959c816d97d11b44e007eca00d04ae2be1c6
Philipp W <74567206+pwagnerde@users.noreply.github.com>
2021-01-28 20:49:29 +0100

    Reddits links added

---
+++ src/constants/base.js
@@ -0,0 +1,12 @@
+export const API_URL = 'https://www.reddit.com'
+
+export const REDDITS = [
+  'userexperience', // 73k members
+  'design', //1.9m members
+  'futurology', // 15.1m members
+  'technology', // 10.1m members
+  'dataisbeautiful', // 15.4m members
+  'reactjs', //235k members
+  'singularity', // 100k members
+  'golf', // 336k members
+]
