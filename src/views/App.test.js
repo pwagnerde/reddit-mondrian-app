894b47402e4305652140392b1653620280e28b4e
Philipp W <74567206+pwagnerde@users.noreply.github.com>
2021-01-31 15:41:40 +0100

    #6 Jest test added

--- src/views/App.test.js
+++ src/views/App.test.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from '../stores/rootStore';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import store from '../stores/rootStore'
+import App from './App'
+import ReactDOM from 'react-dom'
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+describe('app.js', () => {
+  test('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div
+    )
+  })
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+  it('renders header component', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    expect(screen.getByText('Mondrian')).toBeInTheDocument()
+  })
+
+    it('renders footer component', () => {
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      )
+      expect(screen.getByText('Copyright')).toBeInTheDocument()
+    })
+})
